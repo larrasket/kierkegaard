299bff5eacf7afd726f1d58696a974094703774a
Salih Muhammed <root@lr0.org>
2026-01-13 23:42:15 +0200

    Fix refresh tokens

--- src/main/scala/kierkegaard/twitter/auth/Credentials.scala
+++ src/main/scala/kierkegaard/twitter/auth/Credentials.scala
@@ -1,7 +1,13 @@
 package kierkegaard.twitter.auth
 
 import com.typesafe.config.{Config, ConfigFactory}
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 import scala.util.Try
+import java.io.{File, PrintWriter}
+import scala.io.Source
 
 case class OAuth2Credentials(
   clientId: String,
@@ -18,7 +24,14 @@ case class TwitterCredentials(
   tokens: Option[AccessTokens]
 )
 
+case class PersistedTokens(
+  accessToken: String,
+  refreshToken: Option[String]
+)
+
 object TwitterCredentials {
+  
+  private val TOKENS_FILE = "data/tokens.json"
   
   def load(): Either[String, TwitterCredentials] = {
     Try {
@@ -27,42 +40,44 @@ object TwitterCredentials {
       val clientId = config.getString("twitter.oauth2.clientId")
       val clientSecret = config.getString("twitter.oauth2.clientSecret")
       
-      val accessToken = Try(config.getString("twitter.tokens.accessToken")).toOption
-        .filter(_.nonEmpty)
-      val refreshToken = Try(config.getString("twitter.tokens.refreshToken")).toOption
-        .filter(_.nonEmpty)
+      val tokens = loadPersistedTokens().orElse {
+        val accessToken = Try(config.getString("twitter.tokens.accessToken")).toOption
+          .filter(_.nonEmpty)
+        val refreshToken = Try(config.getString("twitter.tokens.refreshToken")).toOption
+          .filter(_.nonEmpty)
+        accessToken.map(token => AccessTokens(token, refreshToken))
+      }
       
       val oauth2 = OAuth2Credentials(clientId, clientSecret)
-      val tokens = accessToken.map(token => AccessTokens(token, refreshToken))
-      
       TwitterCredentials(oauth2, tokens)
     }.toEither.left.map(_.getMessage)
   }
   
-  def saveAccessTokens(accessToken: String, refreshToken: Option[String]): Either[String, Unit] = {
-    import java.nio.file.{Files, Paths}
-    import java.nio.charset.StandardCharsets
+  private def loadPersistedTokens(): Option[AccessTokens] = {
+    val file = new File(TOKENS_FILE)
+    if (!file.exists()) return None
     
     Try {
-      val configPath = Paths.get("src/main/resources/application.conf")
-      val content = new String(Files.readAllBytes(configPath), StandardCharsets.UTF_8)
-      
-      // Replace accessToken = "anything" with the new token
-      val withAccessToken = content.replaceFirst(
-        """(accessToken = )"[^"]*"""",
-        s"""$$1"$accessToken""""
-      )
-      
-      val updated = refreshToken match {
-        case Some(refresh) => withAccessToken.replaceFirst(
-          """(refreshToken = )"[^"]*"""",
-          s"""$$1"$refresh""""
-        )
-        case None => withAccessToken
+      val content = Source.fromFile(file, "UTF-8").mkString
+      decode[PersistedTokens](content).toOption.map { pt =>
+        AccessTokens(pt.accessToken, pt.refreshToken)
       }
+    }.toOption.flatten
+  }
+  
+  def saveAccessTokens(accessToken: String, refreshToken: Option[String]): Either[String, Unit] = {
+    Try {
+      val dataDir = new File("data")
+      if (!dataDir.exists()) dataDir.mkdirs()
       
-      Files.write(configPath, updated.getBytes(StandardCharsets.UTF_8))
-      println(s"Tokens written to $configPath")
+      val tokens = PersistedTokens(accessToken, refreshToken)
+      val writer = new PrintWriter(new File(TOKENS_FILE), "UTF-8")
+      try {
+        writer.write(tokens.asJson.spaces2)
+      } finally {
+        writer.close()
+      }
+      println(s"Tokens saved to $TOKENS_FILE")
       ()
     }.toEither.left.map(_.getMessage)
   }
